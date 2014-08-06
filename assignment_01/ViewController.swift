634b41738f8e68745a113aad6df795cdc158f1c7
Zack Walkingstick <zwalkingstick@ubermind.com>
2014-08-05 21:33:59 -0700

    Initial Commit

---
+++ assignment_01/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assignment_01
+//
+//  Created by Zack Walkingstick on 8/5/14.
+//  Copyright (c) 2014 Zack_Walkingstick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
